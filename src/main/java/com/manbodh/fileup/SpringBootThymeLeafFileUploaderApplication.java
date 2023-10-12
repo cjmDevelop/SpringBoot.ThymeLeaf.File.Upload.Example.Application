79b7f29e38e7d8b2715b44d1b37a6b7ceeed244e
cjmDev <manbodhclaude@yahoo.com>
2023-10-12 12:32:47 -0400

    intial project files

---
+++ src/main/java/com/manbodh/fileup/SpringBootThymeLeafFileUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.manbodh.fileup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeLeafFileUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeLeafFileUploaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manbodh/fileup/SpringBootThymeLeafFileUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manbodh.fileup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootThymeLeafFileUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
