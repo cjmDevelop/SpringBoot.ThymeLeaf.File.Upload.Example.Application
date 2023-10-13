367372f5f02cc17868b9d17023eb3c69ca824d39
cjmDev <manbodhclaude@yahoo.com>
2023-10-13 03:44:47 -0400

    storage: properties, service, &  exception files

--- src/main/java/com/manbodh/fileup/FileUploadController.java
+++ src/main/java/com/manbodh/fileup/FileUploadController.java
@@ -1,11 +1,17 @@
 package com.manbodh.fileup;
 
+import com.manbodh.fileup.storage.StorageFileNotFoundException;
+import com.manbodh.fileup.storage.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.thymeleaf.spring6.expression.Mvc;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
 import java.util.stream.Collectors;
@@ -31,6 +37,31 @@ public class FileUploadController {
             return "uploadForm";
         }
 
+        @GetMapping("/files/{filename:.+}")
+        @ResponseBody
+        public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+            Resource file = storageService.loadAsResource(filename);
+            if(file == null)
+                return ResponseEntity.notFound().build();
+            return  ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                    "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+
+        }
+
+        @PostMapping("/")
+        public String handleFileUpload(@RequestParam("file") MultipartFile file,
+                                       RedirectAttributes redirectAttributes) {
+            storageService.store(file);
+            redirectAttributes.addFlashAttribute("message", "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return "redirect:/";
+
+        }
+
+        @ExceptionHandler(StorageFileNotFoundException.class)
+        public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+            return ResponseEntity.notFound().build();
+        }
 
 
 }
